34b6e3e044e2dd55c9ade41955125f823501c450
Ruslan Denislamov <ruslan.denislamov@mail.ru>
2023-08-02 19:17:24 +0700

    problem vue template

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello, Vue 3 with Webpack 5!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+/* Ваши стили здесь */
+</style>
---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import '../scss/app.scss';
+
+/* Your JS Code goes here */
+import { createApp } from 'vue';
+import App from '../App.vue';
+
+createApp(App).mount('#app');
+/* Demo JS */
+import './demo.js';
