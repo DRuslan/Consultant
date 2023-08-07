f6baf531370a59eca9a8387d5b6f31e0bad363ae
Denislamov Ruslan <ruslan.denislamov@mail.ru>
2023-08-07 18:04:19 +0700

    layout consultant

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div>
     <h1>Hello, Vue 3 with Webpack 5!</h1>
     <h2>{{ $widjet(widjetParams) }}</h2>
+    <div class="widjet">
+      <a class="widjet__link" href="#">Krible</a>
+      <a class="widjet__link" href="#">Krible</a>
+      <div class="widjet__inner">
+
+      </div>
+    </div>
     <panel />
   </div>
 </template>
@@ -21,3 +28,14 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+  .widjet {
+    width: 100%;
+    height: 50px;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    background: red;
+  }
+</style>
