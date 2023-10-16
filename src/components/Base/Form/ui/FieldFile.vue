95f57032aef4b6e3e9159a591e7fe14f02f6de3b
Ruslan <ruslan.denislamov@mail.ru>
2023-10-16 23:51:34 +0700

    chat button file fix

--- src/components/Base/Form/ui/FieldFile.vue
+++ src/components/Base/Form/ui/FieldFile.vue
@@ -1,137 +1,185 @@
 <template>
-    <div class="input-file">
-      <div 
-      class="input-file__button" 
-      :class="[
-          `${disabled ? 'input-file_disabled' : '' }`
-        ]">
-        <label :style="{ color: $widjet().global.dopPrimary }">
-          <input type="file" accept="image/*,application/pdf" @change="onFileSelected">
-          <Icon size="s" iconName="pin"/>
-            <p v-if="title">Прикрепить файл</p>
-        </label>
-        <p v-if="selectedFile != null" class="input-file__name t_small" :style="{ color: $widjet().global.dopPrimary }">{{getFileName}}</p>
-      </div>
+  <div class="input-file">
+    <div
+      class="input-file__button"
+      :class="[`${disabled ? 'input-file_disabled' : ''}`]"
+    >
+      <label :style="{ color: $widjet().global.dopPrimary }">
+        <input
+          type="file"
+          accept="image/*,application/pdf"
+          @input="onInput"
+          :disabled="disabled"
+        />
+        <Icon size="s" iconName="pin" />
+        <p v-if="title">Прикрепить файл</p>
+      </label>
+      <p
+        v-if="selectedFile != null"
+        class="input-file__name t_small"
+        :style="{ color: $widjet().global.dopPrimary }"
+      >
+        {{ getFileName }}
+      </p>
     </div>
-  </template>
+  </div>
+</template>
   
 <script>
-  import Icon from "../../Icon.vue";
-  export default {
-    name: "InputFile",
-    components: {Icon},
-    props: {
-      value: {
-        type: Object,
-        default: null,
-      },
-      title: {
-        type: Boolean,
-        default: true,
-      }, 
-      disabled: {
-        type: Boolean,
-        default: false,
+import Icon from "../../Icon.vue";
+export default {
+  name: "InputFile",
+  components: { Icon },
+  props: {
+    value: {
+      type: Object,
+      default: null,
+    },
+    title: {
+      type: Boolean,
+      default: true,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      selectedFile: null,
+    };
+  },
+  watch: {
+    value(newVal, oldVal) {
+      this.selectedFile = newVal;
+    },
+  },
+  methods: {
+    onInput(event) {
+      if (this.disabled) {
+        event.preventDefault();
+        event.target.value = null;
+        console.log('[1]');
+      } else {
+        console.log('[2]');
+        this.onFileSelected(event);
       }
     },
-    data() {
-      return {
-        selectedFile: null,
-      }
-    },
-    watch: {
-      value(newVal, oldVal) {
-        this.selectedFile = newVal;
-      },
-    },
-    methods: {
-      onFileSelected(event) {
-        if (this.disabled) {
-          event.preventDefault();
-        }
-
-        const file = event.target.files[0];
-        if (file) {
-          const fileType = file.type;
-          if (
-            fileType === 'image/jpeg'
-            || fileType === 'image/jpg'
-            || fileType === 'image/png'
-            || fileType === 'image/heic'
-            || fileType === 'application/pdf'
-          ) {
-            this.selectedFile = file;
-            alert('Файл успешно выбран');
-          } else {
-            alert('Пожалуйста, выберите картинку (jpeg, jpg, png, heic) или файл PDF')
-            this.selectedFile = null;
-          }
+    onFileSelected(event) {
+      const file = event.target.files[0];
+      if (file) {
+        const fileType = file.type;
+        if (
+          fileType === "image/jpeg" ||
+          fileType === "image/jpg" ||
+          fileType === "image/png" ||
+          fileType === "image/heic" ||
+          fileType === "application/pdf"
+        ) {
+          this.selectedFile = file;
+          alert("Файл успешно выбран");
         } else {
+          alert(
+            "Пожалуйста, выберите картинку (jpeg, jpg, png, heic) или файл PDF"
+          );
           this.selectedFile = null;
         }
-        this.$emit('update:modelValue', this.selectedFile);
-      },
-    },
-    computed: {
-      getFileName() {
-        if (this.selectedFile == null) return "";
-        return this.selectedFile.name;
+      } else {
+        this.selectedFile = null;
       }
-    }
-  }
-  </script>
+      this.$emit("update:modelValue", this.selectedFile);
+    },
+    // onFileSelected(event) {
+    //   const file = event.target.files[0];
+    //   if (file) {
+    //     const fileType = file.type;
+    //     if (
+    //       fileType === "image/jpeg" ||
+    //       fileType === "image/jpg" ||
+    //       fileType === "image/png" ||
+    //       fileType === "image/heic" ||
+    //       fileType === "application/pdf"
+    //     ) {
+    //       this.selectedFile = file;
+    //       alert("Файл успешно выбран");
+    //     } else {
+    //       alert(
+    //         "Пожалуйста, выберите картинку (jpeg, jpg, png, heic) или файл PDF"
+    //       );
+    //       this.selectedFile = null;
+    //     }
+    //   } else {
+    //     this.selectedFile = null;
+    //   }
+    //   this.$emit("update:modelValue", this.selectedFile);
+    // },
+  },
+  computed: {
+    getFileName() {
+      if (this.selectedFile == null) return "";
+      return this.selectedFile.name;
+    },
+  },
+};
+</script>
   
 <style scoped lang="scss">
-  .input-file{
-    position: relative;
-    &_disabled {cursor: not-allowed;}
-    &__button{
-      overflow: hidden;
-      border-radius: 4px;
-      border: 0;
+.input-file {
+  position: relative;
+  &__button {
+    overflow: hidden;
+    border-radius: 4px;
+    border: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    transition: all 0.3s linear;
+
+    &:hover {
+      cursor: pointer;
+    }
+
+    input[type="file"] {
+      display: none;
+    }
+
+    label {
+      cursor: inherit;
       display: flex;
       align-items: center;
-      justify-content: center;
-      width: 100%;
-      height: 100%;
-      transition: all .3s linear;
-  
-      &:hover {
-        cursor: pointer;
-      }
-  
-      input[type="file"]{
-        display: none;
-      }
-  
-      label {
-        cursor: inherit;
-        display: flex;
-        align-items: center;
-  
-        .v-icon{
-          height: 24px;
-          width: 18px;
-        }
-      }
 
-      p {
-        font-size: 10px;
-        margin-left: 6px;
+      .v-icon {
+        height: 24px;
+        width: 18px;
       }
     }
-  
-    &__name{
-      position: absolute;
-      bottom: -15px;
-      right: 0;
-      text-align: right;
-      width: 250px;
-      height: 10px;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      font-size: 12px;
+
+    p {
+      font-size: 10px;
+      margin-left: 6px;
     }
   }
-  </style>
+
+  &_disabled {
+    cursor: not-allowed;
+    &:hover {
+      cursor: not-allowed;
+    }
+  }
+
+  &__name {
+    position: absolute;
+    bottom: -15px;
+    right: 0;
+    text-align: right;
+    width: 250px;
+    height: 10px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-size: 12px;
+  }
+}
+</style>
