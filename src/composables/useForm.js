caac3f83ba2ade9b2e68bbdcf56f3f8e852ed1e9
Ruslan Denislamov <denislamov96@gmail.com>
2023-12-04 20:57:08 +0700

    ym added

--- src/composables/useForm.js
+++ src/composables/useForm.js
@@ -26,7 +26,7 @@ export default function useForm(form, thank, goal) {
           }
 
           // ловим цели
-          // getYandexGoal(goal);
+          getYandexGoal(goal);
     
           axios
             .post("/api/send-lead/bpm", formData, {withCredentials: true})
